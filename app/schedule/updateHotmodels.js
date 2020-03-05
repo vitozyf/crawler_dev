857412b70df48dc2ddec5dda6eedc6175078c883
zhangyongfeng <535069215@qq.com>
2020-03-05 10:29:32 +0800

    update cron time

--- app/schedule/updateHotmodels.js
+++ app/schedule/updateHotmodels.js
@@ -6,7 +6,7 @@ class UpdateHotmodels extends Subscription {
 	static get schedule() {
     return {
 			// interval: '1m', // 1 分钟间隔
-			cron: '* 3 * * *',
+			cron: '0 3 * * *',
 			// cron: '*/5 * * * * *',
 			type: 'all', // 指定所有的 worker 都需要执行
     };
